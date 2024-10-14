1d7681047db5325aedbf6065b8421c8fc7c5f9e3
sayyedyusuf2 <sayyedyusuf2@gmail.com>
2024-10-14 22:11:00 +0530

    added authentication for hash route

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,9 +2,12 @@ const router = require('express').Router()
 const paymentController = require('../controllers/paymentController')
 const validator = require('../middlewares/validator')
 const schema = require('../validators/index')
+const { validateToken, checkUserExist} = require('../middlewares/auth')
 
 /* Make Payment */
 router.post('/', paymentController.makePayment)
+
+router.use(validateToken, checkUserExist)
 
 /* Hash Payment */
 router.post('/hash', paymentController.hashPayment)
